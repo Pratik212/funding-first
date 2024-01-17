06394f8dc5bb519f706b1e6448d709d1bed65d60
Pratik-Palanpure <pratikpalanpure212@gmail.com>
2024-01-17 12:33:46 +0530

    manage image content.

--- src/app/components/WhyUs.jsx
+++ src/app/components/WhyUs.jsx
@@ -5,15 +5,14 @@ import Facebook from "../../../public/images/why-us1.jpg";
 
 
 const WhyUs = () => {
-
     return (
             <section className="mt-5 mb-2" id="whyus">
                 <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                     Why Us
                 </h2>
-            <div>
-            <Image style={{width: '100%', imageRendering: 'auto'}}  src={Facebook} alt="Facebook Icon"/>
-            </div>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Image src={Facebook} alt="Facebook Icon"/>
+                </div>
         </section>
     );
 };
